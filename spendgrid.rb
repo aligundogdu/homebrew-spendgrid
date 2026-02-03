9631c475db289e2724642741e7d82429e731a48c
aligundogdu <ali@vuska.net>
2026-02-03 11:58:32 +0300

    Update formula with SHA256 hashes for v0.1.0

--- spendgrid.rb
+++ spendgrid.rb
@@ -7,19 +7,19 @@ class Spendgrid < Formula
   # macOS Intel
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/aligundogdu/SpendGrid/releases/download/v0.1.0/spendgrid-darwin-amd64"
-    sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
+    sha256 "c096dc3eb210e2eb53b5b75d6477946b52b05155bc3e6732748ab9dfefe6b2d0"
   end
 
   # macOS Apple Silicon
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/aligundogdu/SpendGrid/releases/download/v0.1.0/spendgrid-darwin-arm64"
-    sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
+    sha256 "e6f003e68277c28fc0f809585977a79dbea9edb8026f74f1cd4038158b32b8f9"
   end
 
   # Linux
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/aligundogdu/SpendGrid/releases/download/v0.1.0/spendgrid-linux-amd64"
-    sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+    sha256 "475e78fef9ea6c520fab0e5a569ec7119355ba1f4b87ee60bb9a57472641a62a"
   end
 
   def install
